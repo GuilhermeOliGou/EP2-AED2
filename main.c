564e7c24acf3770b7aa7bbb02c0ba3e49be52306
GuilhermeOliGou <guilherme.oligou@gmail.com>
2018-07-02 18:33:30 -0300

    Entrada e Saída implementados, falta testar. Remoção é a única faltando implementar.

--- main.c
+++ main.c
@@ -30,7 +30,7 @@ int ProcuraEmNO(NO_ARV_B* no, int ch){
     int i = 0;
     while (i < no->chavesValidas){
         if (no->chaves[i] == ch){
-            printf("Erro! Chave ja existente na arvore!");
+            printf("ERRO! Chave ja existente na arvore!");
             return -1;
         }
         if (no->chaves[i] > ch)
@@ -72,8 +72,6 @@ void QuebraNO(NO_ARV_B** raiz ,NO_ARV_B* pai, NO_ARV_B* filho, int i){
     pai->chavesValidas = pai->chavesValidas+1;
     filho->chavesValidas = filho->chavesValidas-T;
 }
-
-//FUNÇÕES PRINCIPAIS
 
 void InsereEmNO(NO_ARV_B** raiz, NO_ARV_B* no, int ch, bool* modificar){
     if (no->chavesValidas == T*2-1){
@@ -117,44 +115,83 @@ void InsereEmNO(NO_ARV_B** raiz, NO_ARV_B* no, int ch, bool* modificar){
     }
 }
 
+void EscreveArvB (FILE* saida, NO_ARV_B* no){
+    fprintf(saida,"%s","(");
+    int i;
+    for(i = 0; i < no->chavesValidas; i++){
+        if(no->filhos[i]){
+            EscreveArvB(saida, no->filhos[i]);
+            fprintf(saida," ");
+        }
+        fprintf(saida,"%d",no->chaves[i]);
+        if (i < no->chavesValidas -1){
+            fprintf(saida," ");
+        }
+    }
+    if(no->filhos[i]){
+        fprintf(saida," ");
+        EscreveArvB(saida, no->filhos[i]);
+    }
+    fprintf(saida,"%s",")");
+}
+
+//FUNÇÕES PRINCIPAIS
+
 void InsereArvB (NO_ARV_B* raiz, int ch){
     if (ch < 0){
-        printf("Erro! Valor da chave menor que o minimo permitido de 0!\n");
+        printf("ERRO! Valor da chave menor que o minimo permitido de 0!\n");
         return;
     }
     bool modificar = false;
     InsereEmNO(&raiz,raiz,ch,&modificar);
 }
 
-//FUNÇÕES DE TESTE
+void RemoveArvB(NO_ARV_B* raiz, int ch){
+    if (ch < 0){
+        printf("ERRO! Valor da chave menor que o minimo permitido de 0!\n");
+        return;
+    }
 
-void ImprimeArvB (NO_ARV_B* no){
-    printf("(");
-    int i;
-    for(i = 0; i < no->chavesValidas; i++){
-        if(no->filhos[i]){
-            ImprimeArvB(no->filhos[i]);
-            printf(" ");
-        }
-        printf("%d",no->chaves[i]);
-        if (i < no->chavesValidas -1){
-            printf(" ");
+}
+
+void EscreveSaida (char* caminhoSaida, NO_ARV_B* raiz){
+    FILE* saida = fopen(caminhoSaida,"w");
+    EscreveArvB(saida,raiz);
+    fclose(saida);
+}
+
+void LeEntrada (char* caminhoEntrada, char* caminhoSaida, NO_ARV_B* raiz){
+    FILE* entrada = fopen(caminhoEntrada,"rt");
+    int ch;
+    char comando;
+
+    while (true){
+        fscanf(entrada,"%c",&comando);
+        switch(comando){
+        case 'f':
+            return;
+        case 'i':
+            fscanf(entrada," %d\n",&ch);
+            InsereArvB(raiz,ch);
+            break;
+        case 'p':
+            EscreveSaida(caminhoSaida,raiz);
+            break;
+        case EOF:
+            return;
+        default:
+            printf("ERRO! Entrada invalida!");
+            break;
         }
     }
-    if(no->filhos[i]){
-        printf(" ");
-        ImprimeArvB(no->filhos[i]);
-    }
-    printf(")");
+    fclose(entrada);
 }
 
 //MAIN
 
-int main(){
+int main(int argc, char** argv){
     NO_ARV_B teste;
     InicializaArvB(&teste);
-    InsereArvB(&teste,3);
-    InsereArvB(&teste,1);
-    ImprimeArvB(&teste);
+    LeEntrada(argv[1], argv[2],&teste);
     return 0;
 }
